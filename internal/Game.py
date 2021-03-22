4c201020bdc3bd5b99323f69c86a9b08caa81a7d
bitqr <abdlbitar@gmail.com>
2021-03-22 04:12:18 +0100

    improve game class

--- internal/Game.py
+++ internal/Game.py
@@ -1,8 +1,11 @@
 from internal.Board import Board
+from internal.GameResult import GameResult
 
 
 class Game:
 
     def __init__(self):
         self.move_history = []
+        self.captured_pieces = dict()
         self.board = Board(8)
+        self.result = GameResult.UNDEFINED
---
+++ internal/GameResult.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class GameResult(Enum):
+    WHITE_WINS = 1
+    BLACK_WINS = 2
+    DRAW = 3
+    UNDEFINED = 4
